bd322b2bd7608283820431fb791a2f931130d684
Kmbaria <kmbaria.george@gmail.com>
2018-08-10 11:04:38 +0300

    https://github.com/Kmbaria/Triangle-tracker-.git

---
+++ script.js
@@ -0,0 +1,19 @@
+function gettriangle(){
+var length=parseFloat(prompt("side length"));
+var width=parseFloat(prompt("side width"));
+var height=parseFloat(prompt("side height"));
+
+if(length===width && length===height && width===height){
+alert("equilateral tringle");
+}
+else if(length===width || length===height || width===height){
+alert("isoceles triangle");
+}
+else if((length+width)<=height || (length+height)<=width || (width+height)<=length){
+alert("Invalid Dimention. NOT a Triangle");
+}
+else{
+alert("scalar");
+}
+}
+gettriangle();
